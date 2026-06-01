4de9ac99e1d8536aba3cdc10556fd3a18c47150b
icey1287 <1287889630@qq.com>
2026-06-01 23:06:37 +0800

    移除study文件夹

--- langchain-study/02SmartTextGenerator.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
-from pydantic import BaseModel, Field
-from typing import List, Literal
-import json
-from datetime import datetime
-
-load_dotenv()
